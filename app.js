cc796211b7e55594a516308820a9d5f72ee01ce2
Ben <ringerben1@gmail.com>
2022-04-12 16:49:17 -0400

    first push

---
+++ app.js
@@ -0,0 +1,38 @@
+/*
+2. Create a function that returns the sum of two numbers that are arguments. Then console.log the function with the variables from step two as your two arguments.
+
+3. Create a function that returns the difference of two numbers that are arguments. Then console.log the function with the variables from step two as your two arguments.
+
+4. Create a function that returns two arguments multiplied by each other. Then console.log the function with the variables from step two as your two arguments.
+
+5. Create a function that returns divides the first argument by the second argument and returns the new value. Then console.log the function with the variables from step two as your two arguments.
+
+6. Once you are done, deploy your project on GitHub and submit a link to your repository.*/
+
+var a = 10;
+var b = 20;
+var c = 2;
+var d = 3;
+var e = 10;
+var f = 5;
+
+function addSum (param1, param2) {
+    console.log(param1 + param2);
+
+}
+addSum(a,b);
+
+function difference (param1, param2){
+    console.log(param1 - param2);
+}
+difference(a,b);
+
+function product (param1, param2){
+    console.log(param1 * param2);
+}
+product(c,d);
+
+function quotient (param1, param2){
+    console.log(param1 / param2);
+}
+quotient(e,c);
